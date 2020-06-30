f6b0e147c29b1da5ce7c69bed80355d02e4afe78
Lucas Silva <lucash.silva1612@gmail.com>
2020-06-29 22:28:44 -0300

    url fix

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,6 +1,7 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+var_dump($_POST);die();
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message'])) {
   http_response_code(500);
   exit();
 }
